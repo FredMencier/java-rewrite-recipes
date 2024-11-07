9d7aaf9722fb63c49b62cb00e91fbc48df10b88f
Frédéric MENCIER <fmencier@yahoo.fr>
2024-11-07 12:50:16 +0100

    Update CheckJavax.java

--- rewrite-springboot-recipes/src/main/java/org/refactor/java/CheckJavax.java
+++ rewrite-springboot-recipes/src/main/java/org/refactor/java/CheckJavax.java
@@ -1,4 +1,41 @@
 package org.refactor.java;
 
-public class CheckJavax {
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Recipe;
+import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.tree.J;
+import org.openrewrite.marker.SearchResult;
+
+import java.time.Duration;
+
+public class CheckJavax extends Recipe {
+
+    @Override
+    public String getDisplayName() {
+        return "Check if javax.* (except javax.ejb.*) is present in source code";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Check if javax.* (except javax.ejb.*) is present in source code.";
+    }
+
+    @Override
+    public Duration getEstimatedEffortPerOccurrence() {
+        return Duration.ofMinutes(10);
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new JavaIsoVisitor<>() {
+
+            public J.Import visitImport(J.Import anImport, ExecutionContext executionContext) {
+                if (anImport.getTypeName().startsWith("javax") && !anImport.getTypeName().startsWith("javax.ejb")) {
+                    return SearchResult.found(anImport, "//FIXME javax");
+                }
+                return super.visitImport(anImport, executionContext);
+            }
+        };
+    }
 }
