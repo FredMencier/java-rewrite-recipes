e901fefec353cdecba336a2de92a59bc887c3903
Frédéric MENCIER <fmencier@yahoo.fr>
2024-11-07 12:50:56 +0100

    Update CheckJavaxTest.java

--- rewrite-springboot-recipes/src/test/java/org/refactor/java/CheckJavaxTest.java
+++ rewrite-springboot-recipes/src/test/java/org/refactor/java/CheckJavaxTest.java
@@ -1,7 +1,56 @@
 package org.refactor.java;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
 
-class CheckJavaxTest {
+import static org.openrewrite.java.Assertions.java;
+
+class CheckJavaxTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new CheckJavax());
+    }
+
+    @Test
+    void foundJavax() {
+        //language=java
+        rewriteRun(
+                java(
+                        """
+                                import javax.persistence.EntityManager;
+                                class Test {
+                                }
+                                """,
+                        """
+                                /*~~(//FIXME javax)~~>*/import javax.persistence.EntityManager;
+                                class Test {
+                                }
+                                """
+                )
+        );
+    }
+
+    @Test
+    void notFoundJavax() {
+        //language=java
+        rewriteRun(
+                java(
+                        """
+                                import java.util.Date;
+                                class Test {
+                                }
+                                """
+                ),
+                java(
+                        """
+                                import javax.ejb.EJB;
+                                class Test2 {
+                                }
+                                """
+                )
+        );
+    }
 
 }
