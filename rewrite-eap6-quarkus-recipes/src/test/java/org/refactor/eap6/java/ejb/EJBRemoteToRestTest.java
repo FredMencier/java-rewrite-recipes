7ed1245b621635631a1403f723b871de56e5719d
Frédéric MENCIER <fmencier@yahoo.fr>
2025-03-07 15:25:33 +0100

    Update EJBRemoteToRestTest.java

--- rewrite-eap6-quarkus-recipes/src/test/java/org/refactor/eap6/java/ejb/EJBRemoteToRestTest.java
+++ rewrite-eap6-quarkus-recipes/src/test/java/org/refactor/eap6/java/ejb/EJBRemoteToRestTest.java
@@ -1,7 +1,49 @@
 package org.refactor.eap6.java.ejb;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+import org.openrewrite.test.SourceSpecs;
+import org.openrewrite.test.TypeValidation;
 
-class EJBRemoteToRestTest {
+import java.io.File;
+import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.openrewrite.java.Assertions.java;
+
+class EJBRemoteToRestTest implements RewriteTest {
+
+      SourceSpecs myObjects = java("""
+
+            """);
+
+      @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipes(new EJBRemoteToRest(null, null)).typeValidationOptions(TypeValidation.none());
+        spec.parser(JavaParser.fromJavaVersion().classpath("jboss-interceptors-api_1.1_spec", "javax.transaction-api"));
+    }
+
+
+      @Test
+    public void shouldProduceAPIWithWrapperInputAndCurrencyRefResponse() throws IOException {
+        rewriteRun(fiscalDtoObjects, g2CommonObjects, fiscalEnumObjects,
+                java("""
+                        package org.refactor.eap6.svc.ejb;
+                                                
+                        import java.util.Date;
+                        import javax.ejb.Remote;
+                                                        
+                        @Remote
+                        public interface IAnimalService {
+                                                
+                            String getAnimals(String name, Date referenceDate);
+                        }
+                        """, sourceSpecs -> sourceSpecs.path("target/IAnimalService.yaml"))
+        );
+        assertThat(FileUtils.readFileToString(new File("target/IAnimalService.yaml"))).isEqualTo(expectedContractWithoutInputWrapper);
+    }
 }
