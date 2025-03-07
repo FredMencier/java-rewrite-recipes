c194f2e5bb82414cf18dc9d1c2b964c05a487fba
Frédéric MENCIER <fmencier@yahoo.fr>
2025-03-07 14:19:32 +0100

    Update ToRest.java

--- rewrite-eap6-quarkus-recipes/src/main/java/org/refactor/eap6/java/annotation/ToRest.java
+++ rewrite-eap6-quarkus-recipes/src/main/java/org/refactor/eap6/java/annotation/ToRest.java
@@ -1,1 +1,26 @@
+package org.refactor.eap6.java.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ToRest {
+
+    String path() default "";
+
+    ActionType action() default ActionType.POST;
+
+    String inputWrapperName() default "";
+
+    String tag() default "";
+
+    String description() default "";
+
+
+    enum ActionType {
+        GET, POST, PUT, DELETE,
+    }
+}
