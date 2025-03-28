28e881ee3a86285dd16af3d050d851160a99e587
Frédéric MENCIER <fmencier@yahoo.fr>
2025-03-28 16:15:39 +0100

    Update Woman.java

--- rewrite-eap6-quarkus-recipes/src/test/java/org/refactor/eap6/java/dto/Woman.java
+++ rewrite-eap6-quarkus-recipes/src/test/java/org/refactor/eap6/java/dto/Woman.java
@@ -1,1 +1,30 @@
+package org.refactor.eap6.java.dto;
 
+public class Woman implements Human {
+
+    private String hairColor;
+
+    private String name;
+
+
+    public Woman(String hairColor, String name) {
+        this.hairColor = hairColor;
+        this.name = name;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
