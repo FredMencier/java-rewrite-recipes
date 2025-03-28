35041f6129763d7bebc749b33f69f0c4f0a65521
Frédéric MENCIER <fmencier@yahoo.fr>
2025-03-28 16:15:05 +0100

    Update Man.java

--- rewrite-eap6-quarkus-recipes/src/test/java/org/refactor/eap6/java/dto/Man.java
+++ rewrite-eap6-quarkus-recipes/src/test/java/org/refactor/eap6/java/dto/Man.java
@@ -1,1 +1,29 @@
+package org.refactor.eap6.java.dto;
 
+public class Man implements Human {
+
+    private String name;
+
+    private String size;
+
+    public Man(String name, String size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
