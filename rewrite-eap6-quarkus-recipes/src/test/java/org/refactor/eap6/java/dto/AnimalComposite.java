ad4612cd4075c776ff0e2adfccc5f7683622e91c
Frédéric MENCIER <fmencier@yahoo.fr>
2025-03-28 16:13:28 +0100

    Update AnimalComposite.java

--- rewrite-eap6-quarkus-recipes/src/test/java/org/refactor/eap6/java/dto/AnimalComposite.java
+++ rewrite-eap6-quarkus-recipes/src/test/java/org/refactor/eap6/java/dto/AnimalComposite.java
@@ -1,1 +1,31 @@
+package org.refactor.eap6.java.dto;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimalComposite implements Serializable {
+
+    private List<Cat> cats = new ArrayList<>();
+
+    private String code;
+
+    public AnimalComposite() {
+    }
+
+    public List<Cat> getDogs() {
+        return cats;
+    }
+
+    public void setDogs(List<Cat> dogs) {
+        this.cats = cats;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
